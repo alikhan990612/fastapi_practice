3845ad5a8965cdda9c6b99fe1d5741f52bda8d57
Alikhan <alikhan.zhaksylykov@bcc.kz>
2025-05-27 15:41:15 +0500

    added 5 new hotels

--- hotels.py
+++ hotels.py
@@ -4,8 +4,13 @@ from schemas.hotels import Hotel, HotelPatch
 router = APIRouter(prefix="/hotels")
 
 hotels = [
-    {"id": 1, "title": "Royal", "name": "Luxury"},
-    {"id": 2, "title": "Rolex", "name": "Grand"}
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
 ]
 
 @router.get("")
