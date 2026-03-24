a0f5e55c6734fb6213ec628c92fb1cce020b7c86
Alikhan <alikhan990612@gmail.com>
2026-03-25 00:06:59 +0500

    feat: Added BASE_DIR in config file

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+BASE_DIR = Path(__file__).parent.parent
 
 class Settings(BaseSettings):
       DB_NAME: str = None
 
-      model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+      model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", extra="ignore")
 
 settings = Settings()
