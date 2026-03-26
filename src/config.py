219c96a3d62b305f84bcb7b4a3a44857c48c98cc
Alikhan <alikhan990612@gmail.com>
2026-03-26 23:39:54 +0500

    feat: Added resolve() method in config file

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
       DB_NAME: str = None
 
       model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", extra="ignore")
-
+      # print(BaseSettings.__dict__.keys())
 settings = Settings()
