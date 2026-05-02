e4a0d3d4606481347e5135779beb799dd9eaeb9d
Alikhan <alikhan990612@gmail.com>
2026-05-02 23:57:22 +0500

    fix: Deleted extra param and default None value

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
-      DB_NAME: str = None
+      DB_NAME: str
 
-      model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", extra="ignore")
-      # print(BaseSettings.__dict__.keys())
+      model_config = SettingsConfigDict(env_file=BASE_DIR / ".env")
+
 settings = Settings()
