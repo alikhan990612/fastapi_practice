397e41a94955fb4af852efc3129f7e3a3bbac64e
Alikhan <alikhan990612@gmail.com>
2026-09-16 00:04:51 +0500

    fix: Recreated hotel table migration file. Renamed to HotelsOrm, added data into file naming

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from src.config import settings
 from src.database import Base
-from src.models.hotels import HotelOrm
+from src.models.hotels import HotelsOrm
 from src.models.rooms import RoomsOrm
 
 # this is the Alembic Config object, which provides
--- src/models/hotels.py
+++ src/models/hotels.py
@@ -3,9 +3,9 @@ from sqlalchemy import String
 from src.database import Base
 
 
-class HotelOrm(Base):
+class HotelsOrm(Base):
       __tablename__ = "hotels"
 
       id: Mapped[int] = mapped_column(primary_key=True)
-      name: Mapped[str] = mapped_column(String(100))
+      title: Mapped[str] = mapped_column(String(100))
       location: Mapped[str]
